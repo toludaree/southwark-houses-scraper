b2e778e6cdc2bc4fe013f85f86691619d8dfaada
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-13 17:45:03 +0100

    - changed domain to peckham main page

--- southwark_houses/southwark_houses/spiders/peckham.py
+++ southwark_houses/southwark_houses/spiders/peckham.py
@@ -4,7 +4,7 @@ import scrapy
 class PeckhamSpider(scrapy.Spider):
     name = "peckham"
     allowed_domains = ["rightmove.co.uk"]
-    start_urls = ["https://rightmove.co.uk"]
+    start_urls = ["https://www.rightmove.co.uk/house-prices/peckham.html?showMapView=showMapView"]
 
     def parse(self, response):
         pass
