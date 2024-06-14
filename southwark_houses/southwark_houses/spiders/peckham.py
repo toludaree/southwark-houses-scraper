01ebcd423f1c83be83c3796adbbdec9a437f53d2
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-14 11:02:30 +0100

    added functionality to gnerate all records

--- southwark_houses/southwark_houses/spiders/peckham.py
+++ southwark_houses/southwark_houses/spiders/peckham.py
@@ -13,14 +13,22 @@ class PeckhamSpider(scrapy.Spider):
     def parse(self, response):
         options = webdriver.ChromeOptions()
         driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(10)
         driver.get(self.start_urls[0])
-        driver.implicitly_wait(5)
+        
         cookies_element = driver.find_element(By.ID, "onetrust-accept-btn-handler")
         cookies_element.click()
-        time.sleep(1)
+        time.sleep(3)
         year_select_element = driver.find_element(By.XPATH,
             "//div[@class='filter-bar  ']//select[@name='soldIn']")
         Select(year_select_element).select_by_value("1")
-        time.sleep(1)
+        time.sleep(3)
+        extra_records_element = driver.find_elements(By.CLASS_NAME, "expand-toggle")
+        for element in extra_records_element:
+            element.click()
+            time.sleep(1)
+        time.sleep(3)
+
+        
         driver.quit()
 
