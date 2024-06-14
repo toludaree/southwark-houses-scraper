54e75a8f7618f107aec35d61ceefd1e9a243490b
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-14 16:03:58 +0100

    - created a while loop and added the major logic inside the block - added a try-except block to break the loop when there is no next page

--- southwark_houses/southwark_houses/spiders/peckham.py
+++ southwark_houses/southwark_houses/spiders/peckham.py
@@ -7,6 +7,7 @@ from southwark_houses.items import SouthwarkHousesItem
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 
 class PeckhamSpider(Spider):
@@ -27,21 +28,33 @@ class PeckhamSpider(Spider):
             "//div[@class='filter-bar  ']//select[@name='soldIn']")
         Select(year_select_element).select_by_value("1")
         time.sleep(3)
-        extra_records_element = driver.find_elements(By.CLASS_NAME, "expand-toggle")
-        for element in extra_records_element:
-            element.click()
-            time.sleep(1)
-        time.sleep(3)
-        response = driver.page_source
-        driver.quit()
 
-        selector = Selector(text=response)
-        gallery = selector.xpath("//div[@class='propertyCard']")
-        for listing in gallery:
-            item = ItemLoader(item=SouthwarkHousesItem(), response=response, selector=listing)
-            item.add_xpath("address", ".//a[@data-gtm='title']/text()")
-            item.add_xpath("type", ".//div[@class='subTitle bedrooms']/span/text()")
-            item.add_xpath("last_known_price", ".//table//tr[1]/td[@class='price']/text()")
-            item.add_xpath("last_known_tenure", ".//table//tr[1]/td[contains(@class, 'tenure')]/text()")
-            item.add_xpath("transaction_history", ".//table//tr")
-            yield item.load_item()
+        while True:
+            extra_records_element = driver.find_elements(By.CLASS_NAME, "expand-toggle")
+            for element in extra_records_element:
+                element.click()
+                time.sleep(1)
+            time.sleep(3)
+            response = driver.page_source
+
+            selector = Selector(text=response)
+            gallery = selector.xpath("//div[@class='propertyCard']")
+            for listing in gallery:
+                item = ItemLoader(item=SouthwarkHousesItem(), response=response, selector=listing)
+                item.add_xpath("address", ".//a[@data-gtm='title']/text()")
+                item.add_xpath("type", ".//div[@class='subTitle bedrooms']/span/text()")
+                item.add_xpath("last_known_price", ".//table//tr[1]/td[@class='price']/text()")
+                item.add_xpath("last_known_tenure", ".//table//tr[1]/td[contains(@class, 'tenure')]/text()")
+                item.add_xpath("transaction_history", ".//table//tr")
+                yield item.load_item()
+
+            try:
+                next_element = driver.find_element(By.XPATH, "//div[@class='pagination pagination-next ']")
+                next_element.click()
+            except NoSuchElementException:
+                self.logger.info("Last page scraped... Quitting Selenium")
+                break
+
+            time.sleep(5)
+
+        driver.quit()
