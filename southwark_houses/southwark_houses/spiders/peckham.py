63b14a09c32df5c12eed535991d4a532e2f1d8cb
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-14 08:20:16 +0100

    implemented Slenium code to accept cookies and change time frame to the past year

--- southwark_houses/southwark_houses/spiders/peckham.py
+++ southwark_houses/southwark_houses/spiders/peckham.py
@@ -1,4 +1,8 @@
 import scrapy
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 
 class PeckhamSpider(scrapy.Spider):
@@ -7,4 +11,16 @@ class PeckhamSpider(scrapy.Spider):
     start_urls = ["https://www.rightmove.co.uk/house-prices/peckham.html?showMapView=showMapView"]
 
     def parse(self, response):
-        pass
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+        driver.get(self.start_urls[0])
+        driver.implicitly_wait(5)
+        cookies_element = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+        cookies_element.click()
+        time.sleep(1)
+        year_select_element = driver.find_element(By.XPATH,
+            "//div[@class='filter-bar  ']//select[@name='soldIn']")
+        Select(year_select_element).select_by_value("1")
+        time.sleep(1)
+        driver.quit()
+
